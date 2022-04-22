87df6291c279c6a3c8129d79ef77dc7bc3203880
keshavkaartikeya <90041638+keshavkaartikeya@users.noreply.github.com>
2022-04-22 08:36:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+object=[];
+video="";
+status="";
+function preload() {
+    video=createVideo('video.mp4');
+}
+function setup() {
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video.hide();
+}
+function start() {
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status detecting objects";
+}
+function modelLoaded() {
+    console.log("modelLoaded!!");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+function draw() {
+  image(video,0,0,480,380);  
+  if(status !=""){
+  objectDetector.detect(video,gotResult);
+ for (i =0;i < object.lenght;i++) {
+     document.getElementById("status").innerHTML="status: objects detected";
+     document.getElementById("number_of_objects").innerHTML="number of objects detected are:"+objects.lenght;
+     fill("#FF0000");
+     percent=floor(object[i].confidence*100);
+     text(object[i].label+""+percent+"%",object[i].x+15,object[i].y+15);
+     noFill();
+     stroke("#FF0000");
+     rect(object[i].x,object[i].y,object[i].width,object[i].height);
+ }
+
+  }
+}
+function gotResult(error,results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    object=results;
+
+}
